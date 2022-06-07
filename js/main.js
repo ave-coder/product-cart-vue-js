43c3a9fa25afab8f4e0c705db39981380d721a80
Anastasia <nastyadorohina2799@mail.ru>
2022-06-08 00:16:57 +0300

    choice of colour

--- js/main.js
+++ js/main.js
@@ -4,24 +4,32 @@ let app = new Vue({
         product: 'Socks',
         image: "img/green.jpg",
         cart: 0,
-        inStock: true,
-        onSale: false,
+        inStock: true, // наличие товара
+        onSale: false, // наличие скидки
         details: ['80% хлопок', '20% полиэстер', 'Мужские'],
+        size: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         elements: [
             {
                 elemId: 234,
-                elemColor: 'green'
+                elemColor: 'green',
+                elemImg: "https://www.vuemastery.com/images/challenges/vmSocks-green-onWhite.jpg",
+                elemQuantity: 10
             },
             {
                 elemId: 235,
-                elemColor: 'blue'
+                elemColor: 'blue',
+                elemImg: "https://www.vuemastery.com/images/challenges/vmSocks-blue-onWhite.jpg",
+                elemQuantity: 0
             }
         ],
     },
     methods: {
         addCart() {
             this.cart += 1
-        }
+        },
 
+        updateImage(elemImg) {
+            this.image = elemImg; // обновление изображения
+        }
     }
 })
